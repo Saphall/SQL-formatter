803c279a3195504860df1dec2353b33002b7ba9e
Saphall <safalsakha@gmail.com>
2021-10-24 16:52:05 +0545

    Update keywords

--- keywords/keywords.py
+++ keywords/keywords.py
@@ -1,11 +1,11 @@
 """
 List of keywords to change to uppercase.
 """
-KEYWORDS = ['select','create','drop', 'from','where','insert',' int',' varchar',' date','database','table','into','values','group by',
-        'desc','order by',' float',' char',' text',' number',' time',' timestamp','delete','if','exists','in','not',' datetime','add',
-        'constraint','primary key','foreign key','constraints','alter','all','and','any','as','case','when','column','index','between',
+KEYWORDS = ['select','create','drop', 'from','where','insert','int','tinyint','smallint','varchar','date','database','table','into','values','group by',
+        'desc','order by','float','decimal','char','text','number','time','timestamp','delete','if','exists','in','not',' datetime','add',
+        'constraint','primary key','foreign key','constraints','references','using','alter','all','and','any','as','case','when','column','index','between',
         'check','end','when','null','unique','restart','identity','serial','view','default','is','distinct','exec','extract','having','join','outer',
-        'left','inner','right','like','limit','or','procedure',"begin",'rownum','top','update','set','truncate','union','union all','ascii','char_length',
+        'left','inner','right','natural','cross','like','limit','or','procedure',"begin",'rownum','top','update','set','truncate','cascade','union','union all','ascii','char_length',
         'character_length','concat','field','format','cast','replace','locate','ucase','lcase','upper','lower','min','max','sum','avg','count','exp',
         'floor','round','adddate','addtime','datediff','current_date','current_time','current_timestamp','sysdate','time_format',
         'to_days','coalesce','convert','current_user','isnull','ifnull','version','or','full join','check','auto','auto_increment',
